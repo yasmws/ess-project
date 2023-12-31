e48ebdf74b8eb3636d7c6ecb8101ac3305dd2fab
Yasmin Soares <yasminmwsoares@gmail.com>
2023-12-30 23:59:43 -0300

    feat: add home page

--- frontend/aurore/src/app/app.module.ts
+++ frontend/aurore/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route redirects to /home
+  { path: 'home', component: HomeComponent },
+  // Add more routes for other components/pages as needed
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
+
   imports: [
-    BrowserModule
+  BrowserModule,
+  RouterModule.forRoot(
+    routes)
   ],
+
   providers: [],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
