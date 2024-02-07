dabd6472ee251cc738b0eb7733b78b7b5f595511
Sara Lins <sara.lins@luizalabs.com>
2024-02-07 00:15:02 -0300

    chore: create firebase config file

--- backend/users.py
+++ backend/users.py
@@ -1,23 +1,10 @@
 from fastapi import HTTPException
-import pyrebase
-
-firebaseConfig = {
-    'apiKey': "AIzaSyCh_HTUYjfsVZl6HefvuDFAFRwVmry38tw",
-    'authDomain': "aurore-35e60.firebaseapp.com",
-    'projectId': "aurore-35e60",
-    'storageBucket': "aurore-35e60.appspot.com",
-    'messagingSenderId': "194880387139",
-    'appId': "1:194880387139:web:f1502cc327764970607cf6",
-    'databaseURL': "https://aurore-35e60-default-rtdb.firebaseio.com"
-}
-
-firebase = pyrebase.initialize_app(firebaseConfig)
-auth = firebase.auth()
-db = firebase.database()
+import firebase_config
 
 def create_user(email, password, name, username, cpf):
     try:
-        user = auth.create_user_with_email_and_password(email, password)
+        firebase_config.auth.create_user_with_email_and_password(email, password)
+
         print("User created")
         data = {
             "name": name,
@@ -25,7 +12,7 @@ def create_user(email, password, name, username, cpf):
             "cpf": cpf,
             "username": username,
         }
-        db.child("users").child(data["username"]).set(data)
+        firebase_config.db.child("users").child(data["username"]).set(data)
         print("User data added successfully!")
         return "User created!"
     except Exception:
@@ -33,7 +20,7 @@ def create_user(email, password, name, username, cpf):
 
 def login_user(email, password):
     try:
-        user = auth.sign_in_with_email_and_password(email, password)
+        firebase_config.auth.sign_in_with_email_and_password(email, password)
         return "User logged in!"
     except Exception:
         raise HTTPException(status_code=400, detail="Invalid email or password.")
