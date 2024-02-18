4cf26e42d6a0c8e70ba563c1e1e8ad4077b7af2b
RafaelB411 <rafaelb411.rb@gmail.com>
2024-02-18 14:08:05 -0300

    fix(payment_methods): correct json problem to update cnt

--- backend/payment_methods.py
+++ backend/payment_methods.py
@@ -193,7 +193,8 @@ def delete_payment_method(username, method):
                 firebase_config.db.child("payment").child(username).child("method"+str(cur_i)).update(data)
         
         firebase_config.db.child("payment").child(username).child("method"+str(cnt)).set({})
-        firebase_config.db.child("payment").child(username).child("cnt").update(cnt-1)
+        cnt -= 1
+        firebase_config.db.child("payment").child(username).update({"cnt": cnt})
 
     except HTTPException as hex:
         raise hex
