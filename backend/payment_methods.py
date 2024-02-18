d4edec81b6fb4dc36673818a4178d86c4b116ee1
RafaelB411 <rafaelb411.rb@gmail.com>
2024-02-18 02:26:31 -0300

    feat(payment_methods): create add_payment_method function

--- backend/payment_methods.py
+++ backend/payment_methods.py
@@ -2,10 +2,42 @@ from fastapi import HTTPException
 import firebase_config
 import users
 
-#fix def add_payment_method(username, type, key, cnt):
-#    try:
-#
-#    except:
+def add_payment_method(username, type, key):
+    try:        
+        #Validate username
+        registered = False
+        users = firebase_config.db.child("payment").get().val()
+
+        for user in users:
+            if username == user:
+                registered = True
+        
+        if not registered:
+            raise HTTPException(status_code=404, detail="User not found!")
+        
+        #Validate type
+        if type not in {"pix", "debito", "credito"}:
+            raise HTTPException(status_code=400, detail="Invalid payment method!")
+
+        #Check payment method limit
+        cnt = firebase_config.db.child("payment").child(username).child("cnt").get().val()
+        if cnt == 3:
+            raise HTTPException(status_code=400, detail="Payment methods' limit reached!")
+        cnt += 1
+        cnt_s = str(cnt)
+
+        data = {
+            "type":type,
+            "key":key
+        }
+        firebase_config.db.child("payment").child(username).child("cnt").set(cnt)
+        firebase_config.db.child("payment").child(username).child("method"+cnt_s).set(data)
+
+    except HTTPException as hex:
+        raise hex
+    
+    except Exception as ex:
+        raise HTTPException(status_code=500, detail=str(ex))
 
 def register_user(username):
     try: 
