0d2224ac082bc2813a4fc2ef4f9fc41f7214ea36
RafaelB411 <rafaelb411.rb@gmail.com>
2024-02-18 09:29:09 -0300

    feat(payment_methods): create update_payment_method function

--- backend/payment_methods.py
+++ backend/payment_methods.py
@@ -49,7 +49,7 @@ def add_payment_method(username, type, id):
                 method = "method" + str(i)
                 method_type = firebase_config.db.child("payment").child(username).child(method).child("type").get().val()
                 method_id = firebase_config.db.child("payment").child(username).child(method).child("id").get().val()
-                
+
                 if method_type == type and method_id == id:
                     raise HTTPException(status_code=409, detail="This payment method is already registered!")
 
@@ -69,6 +69,108 @@ def add_payment_method(username, type, id):
         }
         firebase_config.db.child("payment").child(username).child("cnt").set(cnt)
         firebase_config.db.child("payment").child(username).child("method"+cnt_s).set(data)
+
+    except HTTPException as hex:
+        raise hex
+    
+    except Exception as ex:
+        raise HTTPException(status_code=500, detail=str(ex))
+    
+
+def update_payment_method(username, method, type, id):
+    try:
+        #Validate username
+        registered = False
+        users = firebase_config.db.child("payment").get().val()
+
+        for user in users:
+            if username == user:
+                registered = True
+        
+        if not registered:
+            raise HTTPException(status_code=404, detail="User not found!")
+        
+        #Validate method
+        registered = False
+        methods = firebase_config.db.child("payment").child(username).get().val()
+
+        for reg_method in methods:
+            if method == reg_method:
+                registered = True
+        
+        if not registered:
+            raise HTTPException(status_code=404, detail="Payment method not found!")
+        
+        #Validate type
+        if type not in {"pix", "boleto", "debito", "credito", "foo"}:
+            raise HTTPException(status_code=400, detail="Invalid payment method!")
+        
+        #Check if payment method is already registered
+        cnt = firebase_config.db.child("payment").child(username).child("cnt").get().val()
+        
+        if cnt != 0:
+            for i in range(cnt+1):
+                key = "method" + str(i)
+                method_type = firebase_config.db.child("payment").child(username).child(key).child("type").get().val()
+                method_id = firebase_config.db.child("payment").child(username).child(key).child("id").get().val()
+
+                if method_id == id and id == None and method_type == type:
+                    raise HTTPException(status_code=409, detail="This payment method is already registered!")
+                elif method_id == id:
+                    raise HTTPException(status_code=409, detail="There is already a payment method with this id registered.")
+
+        #Validate update
+        cur_id = firebase_config.db.child("payment").child(username).child(method).child("id").get().val()
+        cur_type = firebase_config.db.child("payment").child(username).child(method).child("type").get.val()
+
+        if type in {"debito", "credito"}:
+            if id == None:
+                raise HTTPException(status_code=400, detail="This payment method demands an id number.")
+            
+            elif id == "foo":
+                if cur_id == None:
+                    raise HTTPException(status_code=400, detail="This payment method demands an id number.")
+                else:
+                    data = {"type": type,
+                            "id": cur_id}
+            
+            else:
+                splt_str = id.split()
+
+                for string in splt_str:
+                    if len(string) != 4 or len(splt_str) != 4:
+                        raise HTTPException(status_code=400, detail="Stated card number is not valid!")
+
+                data = {"type": type,
+                        "id": id}
+                
+        elif type == "foo":
+            if id == None:
+                if cur_type in {"debito", "credito"}:
+                    raise HTTPException(status_code=400, detail="This payment method demands an id number.")
+                else:
+                    data = {"type": cur_type,
+                            "id": id}
+                    
+            elif id == "foo":
+                data = {"type": cur_type,
+                        "id": cur_id}
+                
+            else:
+                splt_str = id.split()
+
+                for string in splt_str:
+                    if len(string) != 4 or len(splt_str) != 4:
+                        raise HTTPException(status_code=400, detail="Stated card number is not valid!")
+
+                data = {"type": type,
+                        "id": id}
+                
+        else:
+            data = {"type": type,
+                    "id": None}
+        
+        firebase_config.db.child("payment").child(username).child(method).set(data)
 
     except HTTPException as hex:
         raise hex
