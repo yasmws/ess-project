fc439a6bf989a05b3a2c60d6705b9eb5bdbe7745
RafaelB411 <rafaelb411.rb@gmail.com>
2024-02-18 00:54:08 -0300

    fix(payment_methods): solve execution problems from register_user method

--- backend/payment_methods.py
+++ backend/payment_methods.py
@@ -2,7 +2,7 @@ from fastapi import HTTPException
 import firebase_config
 import users
 
-#def add_payment_method(username, type, key, cnt):
+#fix def add_payment_method(username, type, key, cnt):
 #    try:
 #
 #    except:
@@ -10,14 +10,24 @@ import users
 def register_user(username):
     try: 
         registered = False
-        users = firebase_config.db.child("payment")
+        users = firebase_config.db.child("payment").get().val()
+        #print(users)
+
         for user in users:
-            if username == user["username"]:
+            if username == user:
                 registered = True
-        if not registered:
-            data = {"cnt":0}
-            firebase_config.db.child("payment").child(username).set(data)
-    except Exception:
-        raise HTTPException(status_code=409,detail="Usuário já está registrado!")
+
+        if registered:
+            raise HTTPException(status_code=409, detail="User is already registered!")
+
+        data = {"cnt":0}
+        firebase_config.db.child("payment").child(username).set(data)
+        return "User registered!"
+    
+    except HTTPException as hex:
+        raise hex
+
+    except Exception as ex:
+        raise HTTPException(status_code=500, detail=str(ex))
 
 
