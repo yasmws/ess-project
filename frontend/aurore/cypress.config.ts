dd6968db30b34ebd137835bc324f50f42e4fa826
Yasmin Soares <yasminmwsoares@gmail.com>
2024-03-08 04:32:00 -0300

    feat: cypress not working yet

--- frontend/aurore/cypress.config.ts
+++ frontend/aurore/cypress.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
       on(
         "file:preprocessor",
         createBundler({
-          plugins: [createEsbuildPlugin(config)],
+          plugins: [createEsbuildPlugin(config)]
         })
       );
 
