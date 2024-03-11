0212aa66f0428f63ce69ed6d74885cbfb9dd5d20
Sara Lins <sara.lins@luizalabs.com>
2024-03-10 22:02:04 -0300

    feat: create search page and home component

--- frontend/aurore/src/app/app.routing.module.ts
+++ frontend/aurore/src/app/app.routing.module.ts
@@ -2,10 +2,15 @@
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BookingMngtComponent } from './view/booking-mngt/booking-mngt.component'
+import { BookingMngtComponent } from './view/booking-mngt/booking-mngt.component';
+import { SearchPageComponent } from './search-page/search-page.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component:  BookingMngtComponent }
+  { path: '', redirectTo: '/search', pathMatch: 'full' },
+  { path: 'search', component: SearchPageComponent},
+  { path: 'booking-mngt', component:  BookingMngtComponent },
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
