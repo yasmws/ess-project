0c1652515ba01dd52fa32cbf82d36fd3f56712e4
Sara Lins <sara.lins@luizalabs.com>
2024-03-10 22:21:32 -0300

    chore: redirects to home

--- frontend/aurore/src/app/search-page/search-page.component.ts
+++ frontend/aurore/src/app/search-page/search-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-page',
@@ -8,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './search-page.component.css'
 })
 export class SearchPageComponent {
+  location: string = '';
 
+  constructor(private router: Router) { }
+
+  searchAccommodations(): void {
+    // Redirecionar para a página Home
+    this.router.navigate(['/home']);
+  }
 }
