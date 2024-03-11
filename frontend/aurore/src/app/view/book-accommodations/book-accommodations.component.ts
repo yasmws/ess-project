65fd552af2ab394363d28a76ce8e986196e3d1b4
Yasmin Soares <yasminmwsoares@gmail.com>
2024-03-11 20:43:21 -0300

    feat: book-acmdt recebendo e enviando dados entre pags

--- frontend/aurore/src/app/view/book-accommodations/book-accommodations.component.ts
+++ frontend/aurore/src/app/view/book-accommodations/book-accommodations.component.ts
@@ -1,52 +1,46 @@
-import { Component , EventEmitter, OnInit, Output} from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ManegementService } from '../../services/management/management.service';
-import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-book-accommodations',
   templateUrl: './book-accommodations.component.html',
   styleUrl: './book-accommodations.component.css'
 })
+export class BookAccommodationsComponent implements OnInit {
+  id: any;
+  accommodationInfo: any;
+  data: any;
 
-export class BookAccommodationsComponent implements OnInit{
-    id: any;
-    route: any;
-    accommodationInfo: any;
+  constructor(private route: ActivatedRoute, private router: Router, private service: ManegementService) {
+    // Recupera os dados passados pela rota
+    const navigation = this.router.getCurrentNavigation();
+    if (navigation && navigation.extras && navigation.extras.state) {
+      this.data = navigation.extras.state['data'];
+      console.log('Data received from previous page:', this.data);
+    }
+  }
 
-    constructor(private router: Router,private service: ManegementService) {}
-    
-    // Recebe os dados do back
-    ngOnInit(): void {
-      // Retrieve the 'id' parameter from the route
-      //this.route.params.subscribe((params: { [x: string]: any; }) => {
-      //  this.id = params['id'];
-      //  console.log("ID:", this.id);
-        this.id = "5f834504-53b1-48df-9dec-c5beaa3b9dd5";
-        
-        //this.service.getUrlImg(this.id).subscribe((dados: any) => { //NotFound
-        //    console.log("Recebendo dados do back...", dados);
-        //    this.accommodationInfo.image = dados
-        //});
+  ngOnInit(): void {
+    this.service.getAcmdtInfo(this.data['accommodation_id']).subscribe((dados: any) => {
+      console.log("Recebendo dados do back...", dados);
+      // Armazenar os dados recebidos para exibição no template
+      this.accommodationInfo = dados;
+      console.log(this.accommodationInfo);
+    });
+  }
 
-        this.service.getAcmdtInfo(this.id).subscribe((dados: any) => {
-          console.log("Recebendo dados do back...", dados);
-          // Armazenar os dados recebidos p exibição no template
-          
-          this.accommodationInfo = dados
-          console.log(this.accommodationInfo)
-        });
-    };
+  // Modifica cor do botão
+  buttonColor = "#8C271E";
 
-    // Modifica cor do botão
-    buttonColor = "#8C271E";
-    changeButtonColor(color: string): void {
+  changeButtonColor(color: string): void {
     this.buttonColor = color;
-    }
-    //Rota do botão
-    onButtonClick(): void{
-      this.router.navigate(['/payment', { data: JSON.stringify(this.accommodationInfo) }])
-    }
-}
+  }
 
-    
- 
+  // Rota do botão
+  onButtonClick(): void {
+    const data = this.data // Substitua pelos dados reais que você deseja passar
+    console.log('Enviando os seguintes dados para pagamento:', data)
+    this.router.navigate(['/payment'], { state: { data } });
+  }
+}
