cf9321bf94a197d53664101a1d0e88d263f8afae
Yasmin Soares <yasminmwsoares@gmail.com>
2024-03-12 17:20:22 -0300

    feat: payment provisorio

---
+++ frontend/aurore/src/app/view/my-reservations/my-reservations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-reservations',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-reservations.component.html',
+  styleUrl: './my-reservations.component.css'
+})
+export class MyReservationsComponent {
+
+}
--- frontend/aurore/src/app/view/payment/payment.component.ts
+++ frontend/aurore/src/app/view/payment/payment.component.ts
@@ -1,68 +1,37 @@
+import { ManegementService } from 'src/app/services/management/management.service';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { HttpParams } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.css']
+  styleUrl: './payment.component.css'
 })
 export class PaymentComponent implements OnInit {
   data: any;
 
-  constructor(private router: Router, private http: HttpClient) {}
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute, private router: Router, private http: HttpClient, private service: ManegementService){
+    // Recupera os dados passados pela rota
     const navigation = this.router.getCurrentNavigation();
     if (navigation && navigation.extras && navigation.extras.state) {
       this.data = navigation.extras.state['data'];
-      console.log('Data received on the payment page:', this.data);
+      console.log('Data received from previous page:', this.data);
     }
   }
 
-  // Ação quando clica no botão
-  dataReserva: any;
-  createReservation(): void {
-    if (this.data && this.data.valid) {
-      const dataReserva = {
-        reservation_checkin: this.data.checkin,
-        reservation_checkout: this.data.checkout,
-        accommodation_id: this.data.accommodation_id,
-        client_id: this.data.client_id
-      };
-  
-      // Convert dataReserva into URL parameters
-      const params = new HttpParams({ fromObject: dataReserva });
-  
-      this.http
-        .get<any>('http://localhost:8000/reservation/create', { params })
-        .pipe(
-          catchError((error) => {
-            console.error('Error creating reservation:', error);
-            throw error;
-          })
-        )
-        .subscribe({
-          next: (result) => {
-            alert('Reserva criada com sucesso!');
-            console.log('Result:', result);
-  
-            const goToHome = confirm('Deseja ir para a página inicial?');
-            if (goToHome) {
-              this.router.navigate(['/']);
-            } else {
-              this.router.navigate(['/my-reservations']);
-            }
-          },
-          error: (error) => {
-            console.error('Error creating reservation:', error);
-          }
-        });
-    } else {
-      console.error('Data is undefined or does not have the valid property.');
-    }   
+  ngOnInit(): void {
+    // Check if 'data' is defined
+    if (!this.data) {
+      console.error('Data is undefined.');
+    }
   }
+  
+ 
 }
+
