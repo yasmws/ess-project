acf78994642a1256070a889f845098ca4f2aae93
Yasmin Soares <yasminmwsoares@gmail.com>
2024-03-12 17:21:04 -0300

    feat: my-acmdt provisorio

---
+++ frontend/aurore/src/app/view/my-accommodations/my-accommodations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-accommodations',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-accommodations.component.html',
+  styleUrl: './my-accommodations.component.css'
+})
+export class MyAccommodationsComponent {
+
+}
