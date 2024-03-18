b62db5a25f60ce812d96f776ef8560d7e80d699c
Sara Lins <sara.lins@luizalabs.com>
2024-03-18 06:13:54 -0300

    feat: add tests to search page

--- frontend/aurore/tests/features/search.feature
+++ frontend/aurore/tests/features/search.feature
@@ -1,7 +1,18 @@
-Feature: Teste da Página de Busca
+Feature: Busca de Acomodações
 
-Scenario: Verificar se a página de busca está funcionando corretamente
-  Given Eu estou na página de busca
-  When Eu preencho o formulário de busca
-  And Eu clico no botão de busca
-  Then Eu vejo os resultados da busca
+Scenario: Buscar acomodações sem nenhum parâmetro
+    Given Estou na tela "Busca de acomodações"
+    When Clico no botão de busca
+    Then Vejo os resultados da busca com acomodações disponíveis
+
+Scenario: Buscar acomodações disponíveis por localização
+    Given Estou na tela "Busca de acomodações"
+    When Preencho o formulário de busca com Localização "nice"
+    And Clico no botão de busca
+    Then Vejo os resultados da busca com acomodações disponíveis
+
+Scenario: Buscar acomodações com data de check-in maior que check-out
+    Given Estou na tela "Busca de acomodações"
+    When Preencho o formulário de busca com Localização "Recife", Check-in "2024-03-15", Check-out "2024-03-12" e Número de Hóspedes "2"
+    And Clico no botão de busca
+    Then Vejo uma mensagem de erro indicando que a data de check-in é maior que a data de check-out
--- frontend/aurore/tests/step-definition/search.ts
+++ frontend/aurore/tests/step-definition/search.ts
@@ -2,22 +2,34 @@ import { Given, When, Then } from '@cucumber/cucumber';
 import { test, expect } from '@playwright/test';
 import { ICustomWorld } from '../support/custom-world';
 
-Given('Eu estou na página de busca', async function (this: ICustomWorld) {
-  await this.page!.goto('http://localhost:4200/search-page');
+Given('Estou na tela "Busca de acomodações"', async function (this: ICustomWorld) {
+  await this.page!.goto('http://localhost:4200/search');
 });
 
-When('Eu preencho o formulário de busca', {timeout: 60 * 1000}, async function (this: ICustomWorld) {
-  await this.page!.fill('#location', 'Recife');
-  await this.page!.fill('#checkin', '2024-03-12');
-  await this.page!.fill('#checkout', '2024-03-15');
-  await this.page!.fill('#guests', '2');
+When('Clico no botão de busca', async function (this: ICustomWorld) {
+  await this.page!.click('.search-button');
 });
 
-When('Eu clico no botão de busca', async function (this: ICustomWorld) {
-  await this.page!.click('button[type="submit"]');
+Then('Vejo os resultados da busca com acomodações disponíveis', {timeout: 60 * 1000}, async function (this: ICustomWorld) {
+    // Espera até que a URL contenha "/home"
+    await this.page!.waitForURL(/\/home/);
+    await this.page!.waitForLoadState('networkidle');
+
+    const accommodationContainer = await this.page!.$('.accommodations-container');
+    expect(accommodationContainer).not.toBeNull();
 });
 
-Then('Eu vejo os resultados da busca', async function (this: ICustomWorld) {
-  await this.page!.waitForSelector('.search-results');
-  expect(await this.page!.isVisible('.search-results')).toBeTruthy();
+When('Preencho o formulário de busca com Localização {string}', async function (this: ICustomWorld, location: string) {
+  await this.page!.fill('#location', location);
+});
+
+When('Preencho o formulário de busca com Localização {string}, Check-in {string}, Check-out {string} e Número de Hóspedes {string}', {timeout: 60 * 1000}, async function (this: ICustomWorld, location: string, checkin: string, checkout: string, guests: string) {
+  await this.page!.fill('#location', location);
+  await this.page!.fill('#checkin', checkin);
+  await this.page!.fill('#checkout', checkout);
+  await this.page!.fill('#guests', guests);
+});
+
+Then('Vejo uma mensagem de erro indicando que a data de check-in é maior que a data de check-out', {timeout: 60 * 1000}, async function (this: ICustomWorld) {
+  expect(await this.page!.isVisible('.error-message')).toBeTruthy();
 });
